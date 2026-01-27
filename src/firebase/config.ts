3bdf48ac8ab4e087ed8fa2db29f9cf476845d11a
Misrat Mim <misratmim65@gmail.com>
2026-01-27 05:43:15 +0000

    আমি সব কিছু করেছি আবার নতুন ও ঠিক করে তুমি দেখ

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,12 +3,12 @@
 // You can commit this file to your repository.
 // It will be replaced with the actual config during deployment.
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID"
+  apiKey: "REDACTED",
+  authDomain: "afrah-organic-mart.firebaseapp.com",
+  projectId: "afrah-organic-mart",
+  storageBucket: "afrah-organic-mart.appspot.com",
+  messagingSenderId: "REDACTED",
+  appId: "REDACTED"
 };
 
 export default firebaseConfig;
