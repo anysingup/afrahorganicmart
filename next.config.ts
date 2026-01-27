8239ae79073639d79c305d9ef7917d0edd3b8772
Misrat Mim <misratmim65@gmail.com>
2026-01-27 07:05:45 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://static-

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'static-01.daraz.com.bd',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
